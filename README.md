9f083a397202f89cefdfe4f0fd13fc2f0d1b3cf2
s4na <appletea.umauma@gmail.com>
2020-09-26 03:33:48 +0900

    s

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.ruby-lang.org/en/news/2020/09/25/ruby-3-0-0-preview1-released/
---
+++ ractor.rb
@@ -0,0 +1,17 @@
+require 'prime'
+
+# n.prime? with sent integers in r1, r2 run in parallel
+r1, r2 = *(1..2).map do
+  Ractor.new do
+    n = Ractor.recv
+    n.prime?
+  end
+end
+
+# send parameters
+r1.send 2**61 - 1
+r2.send 2**61 + 15
+
+# wait for the results of expr1, expr2
+p r1.take #=> true
+p r2.take #=> true
